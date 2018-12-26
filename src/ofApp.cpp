1be3bf3884bd34a44c9aea829f8505bfa1d51f39
Koen Van der Auwera <koen.vanderauwera@gmail.com>
2018-12-26 18:59:29 +0100

    Add project files.

---
+++ src/ofApp.cpp
@@ -0,0 +1,37 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	ofSetFrameRate(60);
+	ofSetWindowTitle("openFrameworks");
+
+	ofBackground(239);
+	ofSetLineWidth(1.5);
+
+	this->number_of_particle = 39;
+	for (int i = 0; i < this->number_of_particle; i++) {
+
+		auto particle = make_unique<Particle>();
+		this->particles.push_back(move(particle));
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+	for (auto& particle : this->particles) {
+
+		particle->update(this->particles);
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	for (auto& particle : this->particles) {
+
+		particle->draw();
+	}
+}
+
